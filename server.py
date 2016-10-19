3a6ce447bd233661e3fe09f67ae474d5e1a774ad
Ben Workman <ubuntu@Bens-MacBook-Pro.local>
2016-10-19 13:50:02 -0400

    Second Wiki Commit

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ load_dotenv(find_dotenv())
 
 from flask import Flask, render_template, redirect, request
 import pg
+import os
 
 db = pg.DB(
     dbname=os.environ.get('PG_DBNAME'),
@@ -11,18 +12,19 @@ db = pg.DB(
     passwd=os.environ.get('PG_PASSWORD')
 )
 
-app = Flask('Wiki')
+tmp_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask('Wiki', template_folder=tmp_dir)
 
 
 
 @app.route('/<page_name>')
 def view_page(page_name):
     return render_template(
-        'placeholder template'
+        'placeholder template',
         page_name=page_name
     )
 
-@app.route('/<page_name/edit')
+@app.route('/<page_name>/edit')
 def edit_page(page_name):
     return render_template(
         'edit.html',
